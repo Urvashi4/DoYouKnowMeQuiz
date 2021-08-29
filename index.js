26f245e429308a079bfda9595ea728cc5ae89185
Urvashi <Urvashimalik867@gmail.com>
2021-08-30 00:09:55 +0530

    added 2 more question to quiz

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var userName = chalk.yellow.bold(ReadlineSync.question("May i know your Name? \n
 var gameName = chalk.yellow.bold('DO YOU KNOW Urvashi');
 var line1 = chalk.green.bold('==========================');
 
-console.log("Welcome " + userName );
+console.log("Welcome " + userName);
 console.log("Lets play " + gameName + "\n");
 
 console.log(chalk.blue.bold("RULES to play the GAME!!!!"));
@@ -22,13 +22,13 @@ var score = 0;
 
 
 
-function play(question,answer){
+function play(question, answer) {
   var userAnswer = ReadlineSync.question(chalk.blue.bold(question));
 
-  if(userAnswer == answer){
+  if (userAnswer == answer) {
     console.log(chalk.green.bold(" YAY Right!!!"));
-    score++ ;
-  }else{
+    score++;
+  } else {
     console.log(chalk.red.bold("Oopss Wrong Answer!!! Correct answer is " + answer));
   }
 
@@ -37,18 +37,23 @@ function play(question,answer){
 }
 
 var questions = [{
-  question : "What is my full Name? \n a)Urvashi Malik \n b)Urvashi Rautela \n",
-  answer : "a"
-},{
-  question : "Where do i stay? \n a)Delhi \n b)Bangalore \n",
-  answer : "b"
-},{
-  question : "Where do i work? \n a)Amazon \n b)TCS \n",
-  answer : "b"
+  question: "What is my full Name? \n a)Urvashi Malik \n b)Urvashi Rautela \n",
+  answer: "a"
+}, {
+  question: "Where do i stay? \n a)Delhi \n b)Bangalore \n",
+  answer: "b"
+}, {
+  question: "Where do i work? \n a)Amazon \n b)TCS \n",
+  answer: "b"
+}, {
+  question: "What is my favourite programming language? \n a)JavaScript \n b)JAVA \n",
+  answer: "a"
+}, {
+  question: "What is my favourite sport? \n a)Badminton \n b)VolleyBall \n",
 }];
 
-for(var i = 0; i < questions.length ; i++){
-  play(questions[i].question , questions[i].answer);
+for (var i = 0; i < questions.length; i++) {
+  play(questions[i].question, questions[i].answer);
 }
 
 console.log(chalk.blue.bold("YAY Your Final Score is : ") + score + " !!!");
